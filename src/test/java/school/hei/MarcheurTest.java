9d4739db1c3c971723dfdc1f1f1b45b8f48db345
Sun-2302 <hei.dera.932@gmail.com>
2024-06-17 15:23:39 +0300

    test: add doitPasserPar

--- src/test/java/school/hei/MarcheurTest.java
+++ src/test/java/school/hei/MarcheurTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MarcheurTest {
 
@@ -30,7 +29,7 @@ class MarcheurTest {
         var pullmanNexta = new Rue(pullman, nexta);
         var pullmanBalancoire = new Rue("Rue Ranaivo", pullman, balancoire);
         var balancoireBoulevard = new Rue(balancoire, boulevard);
-        var balançoireEsti = new Rue(balancoire, esti);
+        var balancoireEsti = new Rue(balancoire, esti);
         var boulevardEsti = new Rue(boulevard, esti);
 
         //Carte
@@ -64,7 +63,7 @@ class MarcheurTest {
         var pullmanNexta = new Rue(pullman, nexta);
         var pullmanBalancoire = new Rue("Rue Ranaivo", pullman, balancoire);
         var balancoireBoulevard = new Rue(balancoire, boulevard);
-        var balançoireEsti = new Rue(balancoire, esti);
+        var balancoireEsti = new Rue(balancoire, esti);
         var boulevardEsti = new Rue(boulevard, esti);
 
         //Carte
@@ -79,4 +78,37 @@ class MarcheurTest {
         assertEquals(esti, trajet.get(trajet.size() - 1));
     }
 
+    @Test
+    public void doitPasserPar() {
+        //Création des lieux
+        var marais = new Lieu("Marais");
+        var sekolontsika = new Lieu("Sekolontsika");
+        var hei = new Lieu("HEI");
+        var pullman = new Lieu("Pullman");
+        var nexta = new Lieu("Nexta");
+        var balancoire = new Lieu("Balançoire");
+        var boulevard = new Lieu("Boulevard de l'Europe");
+        var esti = new Lieu("ESTI");
+
+        //Création des rues
+        var maraisSekolontsika = new Rue(marais, sekolontsika);
+        var sekolontsikaHei = new Rue(sekolontsika, hei);
+        var heiPullman = new Rue("Rue Andriantsihorana", hei, pullman);
+        var heiBalancoire = new Rue(hei, balancoire);
+        var pullmanNexta = new Rue(pullman, nexta);
+        var pullmanBalancoire = new Rue("Rue Ranaivo", pullman, balancoire);
+        var balancoireBoulevard = new Rue(balancoire, boulevard);
+        var balancoireEsti = new Rue(balancoire, esti);
+        var boulevardEsti = new Rue(boulevard, esti);
+
+        //Carte
+        var carte = new Carte(
+                List.of(marais, sekolontsika, hei, pullman, nexta, balancoire, boulevard, esti)
+        );
+
+        var marcheur = new Marcheur("Bjarni");
+        var trajet = marcheur.marcher(carte, hei, esti).getTrajet();
+
+        assertTrue(trajet.contains(balancoire));
+    }
 }
