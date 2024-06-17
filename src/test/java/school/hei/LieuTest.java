30e23d68898b9fd5e6304cf8bc616a98742afb56
Sun-2302 <hei.dera.932@gmail.com>
2024-06-17 14:52:01 +0300

    test: add LieuTest

---
+++ src/test/java/school/hei/LieuTest.java
@@ -0,0 +1,20 @@
+package school.hei;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LieuTest {
+    @Test
+    public void lieuTest() {
+        var marais = new Lieu("Marais");
+        var sekolontsika = new Lieu("Sekolontsika");
+
+        var maraisSekolontsika = new Rue(marais, sekolontsika);
+
+        marais.ajouterRue(maraisSekolontsika);
+        marais.ajouterRue(maraisSekolontsika);
+
+        assertEquals(1, marais.getRues().size());
+    }
+}
